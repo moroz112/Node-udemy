1726e02d88836d74392ef2c35a6ca710a2aeb527
aleksandrmoroz <aleksandr.moroz@localize.city>
2020-04-10 13:57:41 +0300

    setup for heroku

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -8,7 +8,8 @@ const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
 
-const app = express()
+const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
